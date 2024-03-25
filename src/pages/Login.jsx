5dc2e99cd5fd03755196b1015885959cf4d4bb14
Akash Dabhane <akashdabhane10@gmail.com>
2024-03-25 19:36:41 +0530

    majimprv

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,68 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Context } from '../context/Context';
 import Cookies from 'js-cookie';
-import axios from 'axios';
-import { BASE_URL } from '../services/helper';
+import axios from 'axios'
+import { useFormik } from 'formik'
+import { loginSchema } from '../validationSchema/loginSchema';
+
 
 export default function Login() {
-    const [error, setError] = useState('');
-    const [user, setUser] = useState("");
     const [formData, setFormData] = useState({
         email: '',
         password: ''
     });
+    const [error, setError] = useState('');
+    const [user, setUser] = useState("");
     const { setIsLogin, setEmail } = useContext(Context);
-    const email = useRef(null);
-    const password = useRef(null);
     const navigate = useNavigate();
 
-    const handleButtonClick = () => {
-        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
-        if (emailRegex.test(email.current.value) && password.current.value.length >= 6) {
-            console.log(formData); 
+
+    // using formik for handling input fields
+    const initialValues = {
+        email: "",
+        password: "",
+    };
+
+    // values, handleBlur, handleChange, handleSubmit, errors, touched
+    const formik = useFormik({
+        initialValues,
+        validationSchema: loginSchema,
+        validateOnChange: true,
+        validateOnBlur: false,
+        // By disabling validation onChange and onBlur formik will validate on submit.
+        onSubmit: (values, action) => {
+            console.log(values);
+            setFormData(values);
+
+            // to get rid of all the values after submitting the form
+            action.resetForm();
+        }
+    })
+
+
+    useEffect(() => {
+        if (formData.email !== "" && formData.password !== "") {
             try {
-                axios.post(`${BASE_URL}/login`, formData)
+                axios.post('http://localhost:9000/login', formData)
                     .then(data => {
                         setError('login successful!');
                         navigate("/");
-                        Cookies.set('email', data.data.email, { expires: 1}); 
+                        Cookies.set('email', data.data.email, { expires: 1 });
 
                         Cookies.set('isLogin', true, { expires: 1 });
                         const loginStatus = Cookies.get('isLogin');
                         setIsLogin(loginStatus);
-    
+
                         Cookies.set('user', data.data.name, { expires: 1 });
                         const userStatus = Cookies.get('user');
                         setUser(data.name);
-                        
+
                     })
                     .catch(error => {
                         setError(error);
                     })
-                
+
             } catch (error) {
                 console.log('error occured')
             }
@@ -49,34 +71,40 @@ export default function Login() {
             // setIsLogin(Cookies.get('isLogin'));
             // Cookies.set('isLogin', true, { expires: 1 });    // cookies to store login status, will expire after 1 day
             // Cookies.set('user', user, { expires: 1 });
-
-        } else if (email.current.value.length === 0 && password.current.value.length === 0) {
-            setError('All fields are required!')
-        } else if (!emailRegex.test(email.current.value)) {
-            setError('Invalid email address')
-        } else if (password.current.value.length < 6) {
-            setError('Invalid password')
         }
-    }
 
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-        console.log(formData);
-    }
+    }, [formData])
 
     return (
         <div className='flex justify-center h-[100vh]'>
-            <form className="login w-[95%] md:w-[40%] h-[80vh] rounded-md flex flex-col justify-center p-10 mt-10 space-y-5  bg-[url('https://media.istockphoto.com/id/1132930101/photo/leadership-concept-with-paper-airplanes.jpg?s=612x612&w=0&k=20&c=GOzBz1_t9QscdF2F0ZfRkUghMHF6z4F8E1eFH6pCDRo=')]">
+            <form onSubmit={formik.handleSubmit}
+                className="login w-[95%] md:w-[40%] h-[80vh] rounded-md flex flex-col justify-center p-10 mt-10 space-y-5  bg-[url('https://media.istockphoto.com/id/1132930101/photo/leadership-concept-with-paper-airplanes.jpg?s=612x612&w=0&k=20&c=GOzBz1_t9QscdF2F0ZfRkUghMHF6z4F8E1eFH6pCDRo=')]">
                 <p className='text-center text-red-500 text-lg font-semibold h-4'>{error}</p>
                 <h1 className='text-3xl font-bold'>Login</h1>
-                <input className='p-2 outline-none rounded-sm' type="email" name="email" id="email" placeholder='Enter Email/Mobile Number' onChange={handleInputChange} ref={email} />
-                <input className='p-2 outline-none rounded-sm' type="password" name="password" id="password" autoComplete='off' placeholder='Enter Password' onChange={handleInputChange} ref={password} />
-                <button className='bg-orange-500 text-white font-semibold text-lg py-2 rounded-sm' type='button' onClick={handleButtonClick}>Login</button>
+                <div className="">
+                    <input className='p-2 outline-none rounded-sm w-full' type="email" name="email" id="email" placeholder='Enter Email/Mobile Number'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} />
+
+                    {
+                        formik.touched.email && formik.errors.email ?
+                            <p className="text-red-600">{formik.errors.email}</p>
+                            : null
+                    }
+                </div>
+                <div className="">
+                    <input className='p-2 outline-none rounded-sm w-full' type="password" name="password" id="password" autoComplete='off' placeholder='Enter Password'
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} />
+                    {
+                        formik.touched.password && formik.errors.password ?
+                            <p className="text-red-600">{formik.errors.password}</p>
+                            : null
+                    }
+                </div>
+                <button className='bg-orange-500 text-white font-semibold text-lg py-2 rounded-sm' type='submit'>Login</button>
                 <div className="text-center">Not have account <Link className='text-blue-500 font-semibold' to={"/register"}>Register</Link></div>
             </form>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,62 +1,60 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { BASE_URL } from '../services/helper';
+import { useFormik } from 'formik';
+import { registrationSchema } from '../validationSchema/registrationSchema';
 
 export default function Register() {
+    const [formData, setFormData] = useState(null);
     const [error, setError] = useState('');
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: ''
-    });
-    const name = useRef(null);
-    const email = useRef(null);
-    const password = useRef(null);
-    const confirmPassword = useRef(null);
     const navigate = useNavigate();
 
+    // using formik for handling input fields
+    const initialValues = {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
-        if (name.current.value.length > 2 && emailRegex.test(email.current.value) && password.current.value.length >= 6 && password.current.value === confirmPassword.current.value) {
+    // values, handleBlur, handleChange, handleSubmit, errors, touched
+    const formik = useFormik({
+        initialValues,
+        validationSchema: registrationSchema,
+        validateOnChange: true,
+        validateOnBlur: false,
+        // By disabling validation onChange and onBlur formik will validate on submit.
+        onSubmit: (values, action) => {
+            console.log(values);
+            setFormData(values);
 
-            console.log(formData);
-            axios.post(`${BASE_URL}/register`, formData)
-                .then((data) => {
-                    console.log(data);
-                    setError('registration successful!');
-                    navigate("/login");
-                })
-                .catch((error) => {
-                    alert(error);
-                });
-
-            // upload image to the cloudinary
-            uploadImage();
-
-        } else if (!name.current.value.length > 2) {
-            setError("name is required");
-        } else if (email.current.value.length === 0 && password.current.value.length === 0) {
-            setError('Email and password are required!')
-        } else if (!emailRegex.test(email.current.value)) {
-            setError('Invalid email address')
-        } else if (password.current.value.length < 6) {
-            setError('Invalid password')
-        } else if (!password.current.value === confirmPassword.current.value) {
-            setError('password does not matched')
+            // to get rid of all the values after submitting the form
+            action.resetForm();
         }
-    }
+    })
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
+
+    useEffect(() => {
         console.log(formData);
-    }
+        if (formData !== null) {
+            try {
+                axios.post('http://localhost:9000/register', formData)
+                    .then((data) => {
+                        console.log(data);
+                        setError('registration successful!');
+                        navigate("/login");
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+
+                // upload image to the cloudinary
+                uploadImage();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [])
 
 
     const [imageSelected, setImageSelected] = useState("")
@@ -73,22 +71,69 @@ export default function Register() {
             .catch(error => {
                 console.log(error);
             })
-
     }
 
     return (
         <div className='flex justify-center h-[100vh]'>
-            <form className="register w-[95%] md:w-[40%] h-[80vh] bg-[url('https://media.istockphoto.com/id/1132930101/photo/leadership-concept-with-paper-airplanes.jpg?s=612x612&w=0&k=20&c=GOzBz1_t9QscdF2F0ZfRkUghMHF6z4F8E1eFH6pCDRo=')] rounded-md flex flex-col justify-center p-10 my-10 space-y-5">
+            <form onSubmit={formik.handleSubmit}
+                className="register w-[95%] md:w-[40%] h-[80vh] bg-[url('https://media.istockphoto.com/id/1132930101/photo/leadership-concept-with-paper-airplanes.jpg?s=612x612&w=0&k=20&c=GOzBz1_t9QscdF2F0ZfRkUghMHF6z4F8E1eFH6pCDRo=')] rounded-md flex flex-col justify-center p-10 my-10 space-y-5">
                 <p className='text-center text-red-500 text-lg font-semibold h-4'>{error}</p>
                 <h1 className='text-3xl font-bold'>Register</h1>
-                <input className='p-2 outline-none rounded-sm' type="text" name='name' placeholder='Your name' value={formData.name} onChange={handleInputChange} ref={name} />
-                <input className='p-2 outline-none rounded-sm' type="email" name="email" placeholder='Enter Email' value={formData.email} onChange={handleInputChange} ref={email} />
-                <input className='p-2 outline-none rounded-sm' type="password" name="password" autoComplete='off' placeholder='Enter Password' value={formData.password} onChange={handleInputChange} ref={password} />
-                <input className='p-2 outline-none rounded-sm' type="password" name="confirmPassword" autoComplete='off' id="confirmPassword" placeholder='confirm Password' ref={confirmPassword} />
+                <div className="">
+                    <input className='p-2 outline-none rounded-sm w-full' type="text" name='name' placeholder='Your name'
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} />
+
+                    {
+                        formik.touched.name && formik.errors.name ?
+                            <p className="text-red-600">{formik.errors.name}</p>
+                            : null
+                    }
+
+                </div>
+                <div className="">
+                    <input className='p-2 outline-none rounded-sm w-full' type="email" name="email" placeholder='Enter Email'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} />
+
+                    {
+                        formik.touched.email && formik.errors.email ?
+                            <p className="text-red-600">{formik.errors.email}</p>
+                            : null
+                    }
+                </div>
+                <div className="">
+                    <input className='p-2 outline-none rounded-sm w-full' type="password" name="password" autoComplete='off' placeholder='Enter Password'
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} />
+
+                    {
+                        formik.touched.password && formik.errors.password ?
+                            <p className="text-red-600">{formik.errors.password}</p>
+                            : null
+                    }
+
+                </div>
+                <div className="">
+                    <input className='p-2 outline-none rounded-sm w-full' type="password" name="confirmPassword" autoComplete='off' id="confirmPassword" placeholder='confirm Password'
+                        value={formik.values.confirmPassword}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} />
+
+                    {
+                        formik.touched.confirmPassword && formik.errors.confirmPassword ?
+                            <p className="text-red-600">{formik.errors.confirmPassword}</p>
+                            : null
+                    }
+
+                </div>
                 <input type="file" name="profilePhoto" id="profilePhoto" onChange={(event) => {
                     setImageSelected(event.target.files[0]);
                 }} />
-                <button className='bg-orange-500 text-white font-semibold text-lg py-2 rounded-sm' type='submit' onClick={handleSubmit}>Register</button>
+                <button className='bg-orange-500 text-white font-semibold text-lg py-2 rounded-sm' type='submit'>Register</button>
                 <div className="text-center">Already have account <Link className='text-blue-500 font-semibold' to={"/login"}>Login</Link></div>
             </form>
         </div>
