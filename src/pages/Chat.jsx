6d128dd26c890006ba170a0282471899e50a1e9f
Akash Dabhane <akashdabhane10@gmail.com>
2024-09-17 23:41:21 +0530

    user profile side menu added

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -15,6 +15,7 @@ import TopUserBar from '../components/TopUserBar';
 import '../styles/index.css'
 import { baseUrl } from '../utils/helper';
 import ContactListSkeleton from '../loadingSkeleton/ContactListSkeleton';
+import UserProfilePopup from '../popups/UserProfilePopup';
 // import { Picker } from 'emoji-mart';
 // import 'emoji-mart/css/emoji-mart.css';
 
@@ -30,7 +31,7 @@ export default function Chat({ socket }) {
     const [roomName, setRoomName] = useState("");
     const searchBox = useRef(null);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [showUserProfile, setShowUserProfile] = useState(false);
 
     // use effects 
     useEffect(() => {
@@ -124,58 +125,63 @@ export default function Chat({ socket }) {
 
 
     return (
-        <div className='lg:mx-[10%] md:mx-[1%] md:pt-8 md:pb-8 flex h-[93vh] md:h-[97vh] text-white'>
-            <div className="chats md:w-[40%] lg:w-[36%] hidden md:block bg-slate-700 lg:rounded-l-2xl">
-                <div className="flex items-center p-3 justify-between border-b-2 border-slate-500">
-                    <input className='w-full px-2 p-1 text-md outline-none border-none bg-slate-600 rounded' type="text" name="searchUser" id="searchUser" placeholder='search here...' ref={searchBox} />
-                    <span className='text-lg flex items-center px-2 space-x-2 '>
-                        <BiSearchAlt2 className='w-full cursor-pointer' onClick={() => {
-                            searchBox.current.focus();
-                        }} />
-                        <FiMoreVertical className='text-3xl cursor-pointer' />
-                    </span>
+        <>
+            <div className='lg:mx-[10%] md:mx-[1%] md:pt-8 md:pb-8 flex h-[93vh] md:h-[97vh] text-white'>
+                <div className="chats md:w-[40%] lg:w-[36%] hidden md:block bg-slate-700 lg:rounded-l-2xl">
+                    <div className="flex items-center p-3 justify-between border-b-2 border-slate-500">
+                        <input className='w-full px-2 p-1 text-md outline-none border-none bg-slate-600 rounded' type="text" name="searchUser" id="searchUser" placeholder='search here...' ref={searchBox} />
+                        <span className='text-lg flex items-center px-2 space-x-2 '>
+                            <BiSearchAlt2 className='w-full cursor-pointer' onClick={() => {
+                                searchBox.current.focus();
+                            }} />
+                            <FiMoreVertical className='text-3xl cursor-pointer' onClick={() => setShowUserProfile(true)} />
+                        </span>
+                    </div>
+                    {
+                        isLoading ?
+                            <ContactListSkeleton />
+                            :
+                            <LeftPanel users={users} setChatInfo={setChatInfo} handleChatOnClick={handleChatOnClick} />
+                    }
+                </div>
+                <div className="room bg-slate-500 w-full flex flex-col justify-between lg:rounded-r-2xl">
+                    {
+                        Object.keys(chatInfo).length === 0 ?
+                            (
+                                <div className="text-slate-700 flex flex-col items-center justify-center h-full">
+                                    <IoChatbox className='text-4xl' />
+                                    <p>Send and receive messages using chatapp</p>
+                                    <p className='flex items-center space-x-2'>
+                                        <CiLock className='font-bold' />
+                                        <span>End-to-end encryption</span>
+                                    </p>
+                                </div>
+                            )
+                            :
+                            (
+                                <>
+                                    <TopUserBar userData={userData} chatInfo={chatInfo} setChatInfo={setChatInfo} />
+                                    <ScrollToBottom className="showMessages h-[90%] w-[100%] overflow-x-hidden flex flex-col ">
+                                        <ChatWindow chatMessageList={chatMessageList} chatInfo={chatInfo} />
+                                    </ScrollToBottom>
+                                    <div className="inputs flex items-center border-t-2 bg-slate-700 border-gray-800 space-x-1 p-1 pr-4 lg:rounded-br-2xl">
+                                        <label className='px-4 p-2 cursor-pointer hover:bg-slate-900 rounded-md' htmlFor="emoji"><BsEmojiSmile /></label>
+                                        <input className='hidden' type="file" name="emoji" id="emoji" />
+                                        {/* <Picker/> */}
+                                        <label className='px-4 p-2 cursor-pointer hover:bg-slate-900 rounded-md' htmlFor="file"><AiFillFileAdd /></label>
+                                        <input className='hidden' type="file" name="file" id="file" />
+                                        <input className='w-full p-2 py-1 text-lg rounded text-black outline-none border-none' type="text" placeholder='type here...' value={message} onKeyDown={(event) => event.key === "Enter" && handleSendClick()} onChange={(e) => setMessage(e.target.value)} />
+                                        <BiSend className='text-4xl bg-green-500 p-1 rounded cursor-pointer' onClick={handleSendClick} />
+                                    </div>
+                                </>
+                            )
+                    }
                 </div>
                 {
-                    isLoading ?
-                        <ContactListSkeleton />
-                        :
-                        <LeftPanel users={users} setChatInfo={setChatInfo} handleChatOnClick={handleChatOnClick} />
+                    showUserProfile && <UserProfilePopup closeProfilePopup={() => setShowUserProfile(false)} />
                 }
-            </div>
-            <div className="room bg-slate-500 w-full flex flex-col justify-between lg:rounded-r-2xl">
-                {
-                    Object.keys(chatInfo).length === 0 ?
-                        (
-                            <div className="text-slate-700 flex flex-col items-center justify-center h-full">
-                                <IoChatbox className='text-4xl' />
-                                <p>Send and receive messages using chatapp</p>
-                                <p className='flex items-center space-x-2'>
-                                    <CiLock className='font-bold' />
-                                    <span>End-to-end encryption</span>
-                                </p>
-                            </div>
-                        )
-                        :
-                        (
-                            <>
-                                <TopUserBar userData={userData} chatInfo={chatInfo} setChatInfo={setChatInfo} />
-                                <ScrollToBottom className="showMessages h-[90%] w-[100%] overflow-x-hidden flex flex-col ">
-                                    <ChatWindow chatMessageList={chatMessageList} chatInfo={chatInfo} />
-                                </ScrollToBottom>
-                                <div className="inputs flex items-center border-t-2 bg-slate-700 border-gray-800 space-x-1 p-1 pr-4 lg:rounded-br-2xl">
-                                    <label className='px-4 p-2 cursor-pointer hover:bg-slate-900 rounded-md' htmlFor="emoji"><BsEmojiSmile /></label>
-                                    <input className='hidden' type="file" name="emoji" id="emoji" />
-                                    {/* <Picker/> */}
-                                    <label className='px-4 p-2 cursor-pointer hover:bg-slate-900 rounded-md' htmlFor="file"><AiFillFileAdd /></label>
-                                    <input className='hidden' type="file" name="file" id="file" />
-                                    <input className='w-full p-2 py-1 text-lg rounded text-black outline-none border-none' type="text" placeholder='type here...' value={message} onKeyDown={(event) => event.key === "Enter" && handleSendClick()} onChange={(e) => setMessage(e.target.value)} />
-                                    <BiSend className='text-4xl bg-green-500 p-1 rounded cursor-pointer' onClick={handleSendClick} />
-                                </div>
-                            </>
-                        )
-                }
-            </div>
-        </div >
+            </div >
+        </>
     )
 }
 
---
+++ src/popups/UserProfilePopup.jsx
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { baseUrl } from '../utils/helper';
+import Cookies from 'js-cookie';
+import { IoClose } from "react-icons/io5";
+import { MdOutlineEdit } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
+
+function UserProfilePopup({ closeProfilePopup }) {
+    const [userData, setUserData] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate()
+    const [hoverCSS, setHoverCSS] = useState("");
+
+    useEffect(() => {
+        axios.get(`${baseUrl}/users/${Cookies.get("userId")}`, {
+            withCredentials: true,
+            headers: {
+                'Authorization': `Bearer ${Cookies.get('accessToken')}`,
+            }
+        })
+            .then((response) => {
+                console.log(response.data.data)
+                setUserData(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                setError(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [])
+
+    const handleLogoutClick = () => {
+        Cookies.remove("userId");
+        Cookies.remove("accessToken");
+        Cookies.remove("refreshToken");
+        Cookies.remove("name");
+        Cookies.remove("isAuthenticated");
+
+        navigate("/login");
+    }
+
+    return (
+        <div className='fixed inset-0 bg-slate-600 lg:rounded-l-2xl bacckdrop-blur-sm flex flex-col justify-between md:pt-8 md:px-4 md:pb-10 lg:mx-[10%] md:mx-[1%] md:mt-8 md:mb-80 h-max md:h-max md:w-[40%] lg:w-[21.2%]'
+            onBlurCapture={closeProfilePopup}>
+            <div className="flex justify-end">
+                <IoClose className='text-2xl text-black cursor-pointer' onClick={closeProfilePopup} />
+            </div>
+            <div className="p-4">
+                <main className='py-4 border-b-[1px] border-gray-800 space-y-4'>
+                    <img className={`block hover:opacity-30 rounded-full cursor-pointer ${hoverCSS}`}
+                        src={userData?.profileImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVTtlOwG_6l93Lo3NcGZcQpGx4LXNwa3lF5A&s"}
+                        alt="profile photo" width={100} height={100}
+                    // onMouseEnter={() => setHoverCSS("")}
+                    // onMouseLeave={() => setHoverCSS("")}
+                    />
+                    <div className="flex justify-between items-center">
+                        <input className='bg-transparent outline-slate-800'
+                            type="text" value={userData?.name} contentEditable="false" />
+                        <MdOutlineEdit className='p-2 h-8 w-8 hover:bg-slate-600' />
+                    </div>
+                    <p>
+                        <span className='block text-gray-300'>email</span>
+                        <span>{userData?.email}</span>
+                    </p>
+                </main>
+
+                <button className="bg-red-500 text-black rounded px-2 p-1 my-6 text-lg font-semibold"
+                    onClick={handleLogoutClick}>
+                    Logout
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default UserProfilePopup
